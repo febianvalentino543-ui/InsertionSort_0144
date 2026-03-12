c0abc125b270437ad331f863f467aac3378bd95c
febianvalentino543-ui <febianvalentino543@gmail.com>
2026-03-12 12:24:39 +0700

    menambahkan prosedur display

--- Insertion_sort.cpp
+++ Insertion_sort.cpp
@@ -49,3 +49,15 @@ void insertionSort()
     }
 }
 
+void display(){ 
+    cout << endl;
+    cout << "\n====================";
+    cout << "\nElement Array" << endl;
+    cout << "\n=====================" << endl;
+
+    for (int j = 0; j < n; j++){
+        cout << arr[j] << endl;
+    }
+    cout << endl;
+}
+
